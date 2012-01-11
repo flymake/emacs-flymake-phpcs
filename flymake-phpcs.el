5148d46407e49d91cf1e49409f53b1a08ec9ffe1
Sam Graham <git@illusori.co.uk>
2012-01-11 22:51:29 +0000

    Add GNU License and bequeath copyright to FSF.

--- flymake-phpcs.el
+++ flymake-phpcs.el
@@ -1,10 +1,25 @@
 ;;; flymake-phpcs.el --- Flymake handler for PHP to invoke PHP-CodeSniffer
 ;;
+;; Copyright (C) 2011-2012  Free Software Foundation, Inc.
+;;
 ;; Author: Sam Graham <libflymake-phpcs-emacs BLAHBLAH illusori.co.uk>
 ;; Maintainer: Sam Graham <libflymake-phpcs-emacs BLAHBLAH illusori.co.uk>
 ;; URL: https://github.com/illusori/emacs-flymake-phpcs
-;; Version: 1.0
+;; Version: 1.0.1
 ;; Package-Requires: ((flymake "0.3"))
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;
